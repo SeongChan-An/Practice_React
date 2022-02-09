fcce2d2524d6636a8a77fd4c31179ea0ee1357dd
tjscks0510 <tjscks0510@gmail.com>
2022-02-10 00:12:40 +0900

    [Seongchan, ticket-#] [Title] Thinking, think about limitations. [Description] Learned about the limitations of code as a synchronization that seems asynchronous code. [Relevant] -

--- 00..Basic_Concept/02..Redux_Concept_JS/app.js
+++ 00..Basic_Concept/02..Redux_Concept_JS/app.js
@@ -1,15 +1,20 @@
-import { createStore, actionCreator } from './redux.js';
+import { createStore } from './redux.js';
 import { reducer } from './reducer.js';
 import * as Actions from './actions.js';
 
 const store = createStore(reducer);
 
 store.subscribe(function() {
-  console.log(store.getState());
+  console.log('sub => ', store.getState());
 });
 
 store.dispath(Actions.increase());
+console.log('dis => ', store.getState());
 store.dispath(Actions.increase());
+console.log('dis => ', store.getState());
 store.dispath(Actions.increase());
+console.log('dis => ', store.getState());
 store.dispath(Actions.decrease());
+console.log('dis => ', store.getState());
 store.dispath(Actions.reset());
+console.log('dis => ', store.getState());
